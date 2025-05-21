e2354fd2d66ea12cf246c07bd90f77c1fa0cba12
SAIRANTRI <pathaksairantri@gmail.com>
2025-05-21 08:53:24 +0530

    final refinements

--- src/app/profile/_components/ProfileHeader.tsx
+++ src/app/profile/_components/ProfileHeader.tsx
@@ -20,8 +20,6 @@ interface ProfileHeaderProps {
     _id: Id<"users">;
     _creationTime: number;
     proSince?: number | undefined;
-    lemonSqueezyCustomerId?: string | undefined;
-    lemonSqueezyOrderId?: string | undefined;
     name: string;
     userId: string;
     email: string;
